17dfef3719d42d21ab0ea6da02935e3b70c704f8
MainXome <139065079+MainXome@users.noreply.github.com>
2023-07-18 21:49:23 +0500

    Add files via upload

---
+++ NumberGuesser.py
@@ -0,0 +1,45 @@
+#Random Module to make computer choose a random number
+import random 
+#Computer Guessing and User Guessing
+Computer_Random = random.randint(1,100)
+#Make game look good
+print("\033[1m"+"Welcome to Number Guesser"+"\033[0m")
+print("_________________________")
+#User Input
+User_Guess = int(input("Enter a number from 1 to 100 "))
+
+#Win Conditions
+while User_Guess > Computer_Random:
+    print("The number chosen by the computer is lower")
+    User_Guess = int(input("Enter a number from 1 to 100 "))
+while User_Guess < Computer_Random:
+    print("The number chosen by the computer is higher")
+    User_Guess = int(input("Enter a number from 1 to 100 "))
+if User_Guess == Computer_Random:
+    print("Congratulations, You guessed the right number")
+    
+    
+
+
+
+
+    
+    
+    
+      
+       
+    
+    
+    
+    
+    
+    
+ 
+  
+    
+    
+    
+        
+    
+
+   
